1d9559a9e529f9019f158c86f462a6b1d47ce7f1
Victor Ng <victor@crankycoder.com>
2012-12-11 15:55:59 -0500

    added comments for mock_statsdwriterunner

--- mock_statsdwriterunner.go
+++ mock_statsdwriterunner.go
@@ -1,4 +1,23 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2012
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Victor Ng (vng@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
 package heka_mozsvc_plugins
+
+/*
+This file contains a mock for a WriteRunner that returns StatsdMsg
+instances from RetrieveDataObject.  We only implement the methods for
+WriteRunner and delegate down to the underlying MockWriteRunner.
+*/
 
 import (
 	gomock "code.google.com/p/gomock/gomock"
