1723cee3193abe96ee190db5e6980157fa036212
Victor Ng <victor@crankycoder.com>
2013-01-23 12:47:01 -0500

    lots of clean up of unnecessary attributes bound to the SentryMsg struct

--- sentry.go
+++ sentry.go
@@ -37,20 +37,12 @@ func hmac_sha1(message, key []byte) string {
 type SentryMsg struct {
 	encoded_payload string
 
-	epoch_ts64 float64
-	epoch_time time.Time
-	str_ts     string
+	// needed for auth and signature methods
+	str_ts string
 
-	dsn        string
-	parsed_dsn *url.URL
-
-	auth_header  string
+	parsed_dsn   *url.URL
 	dsn_password string
-
-	prep_error error
-	prep_bool  bool
-
-	data_packet []byte
+	data_packet  []byte
 }
 
 type SentryOutputWriter struct {
@@ -58,45 +50,25 @@ type SentryOutputWriter struct {
 	udpMap map[string]net.Conn
 }
 
-func get_auth_header(protocol float32, signature string, timestamp string, client_id string, api_key string) string {
+func (self *SentryMsg) get_auth_header(protocol float32, client_id string) string {
 	header_tmpl := "Sentry sentry_timestamp=%s, sentry_client=%s, sentry_version=%0.1f, sentry_key=%s"
-	return fmt.Sprintf(header_tmpl, timestamp, client_id, protocol, api_key)
+	return fmt.Sprintf(header_tmpl, self.str_ts, client_id, protocol, self.parsed_dsn.User.Username())
 }
 
-func get_signature(message string, str_ts string, key string) string {
-	return hmac_sha1([]byte(fmt.Sprintf("%s %s", str_ts, message)), []byte(key))
-}
-
-type SentryOutError struct {
-	When time.Time
-	What string
-}
-
-func (e SentryOutError) Error() string {
-	return fmt.Sprintf("%v: %v", e.When, e.What)
-}
-
-type MissingPassword struct {
-}
-
-func (e MissingPassword) Error() string {
-	return "No password was found in the DSN URI"
+func (self *SentryMsg) get_signature() string {
+	return hmac_sha1([]byte(fmt.Sprintf("%s %s", self.str_ts, self.encoded_payload)), []byte(self.dsn_password))
 }
 
 func (self *SentryMsg) compute_auth_header() (string, error) {
 
-	self.dsn_password, self.prep_bool = self.parsed_dsn.User.Password()
-	if !self.prep_bool {
-		return "", MissingPassword{}
+	var prep_bool bool
+
+	self.dsn_password, prep_bool = self.parsed_dsn.User.Password()
+	if !prep_bool {
+		return "", fmt.Errorf("No password was found in the DSN URI")
 	}
 
-	self.str_ts = self.epoch_time.Format(time.RFC3339Nano)
-
-	return get_auth_header(2.0,
-		get_signature(self.encoded_payload, self.str_ts, self.dsn_password),
-		self.str_ts,
-		"raven-go/1.0",
-		self.parsed_dsn.User.Username()), nil
+	return self.get_auth_header(2.0, "raven-go/1.0"), nil
 }
 
 type SentryOutputWriterConfig struct {
@@ -128,31 +100,38 @@ func (self *SentryOutputWriter) ZeroOutData(outData interface{}) {
 
 func (self *SentryOutputWriter) PrepOutData(pack *pipeline.PipelinePack, outData interface{}, timeout *time.Duration) error {
 
+	var prep_error error
+	var prep_bool bool
+	var epoch_ts64 float64
+	var epoch_time time.Time
+	var auth_header string
+	var dsn string
+
 	sentryMsg := outData.(*SentryMsg)
 	sentryMsg.encoded_payload = pack.Message.Payload
-	sentryMsg.epoch_ts64, sentryMsg.prep_bool = pack.Message.Fields["epoch_timestamp"].(float64)
+	epoch_ts64, prep_bool = pack.Message.Fields["epoch_timestamp"].(float64)
 
-	if !sentryMsg.prep_bool {
-		return SentryOutError{time.Now(), "Error parsing epoch_timestamp"}
+	if !prep_bool {
+		return fmt.Errorf("Error parsing epoch_timestamp")
 	}
 
-	sentryMsg.epoch_time = time.Unix(int64(sentryMsg.epoch_ts64),
-		int64((sentryMsg.epoch_ts64-float64(int64(sentryMsg.epoch_ts64)))*1e9))
+	epoch_time = (time.Unix(int64(epoch_ts64), int64((epoch_ts64-float64(int64(epoch_ts64)))*1e9)))
+	sentryMsg.str_ts = epoch_time.Format(time.RFC3339Nano)
 
-	sentryMsg.dsn = pack.Message.Fields["dsn"].(string)
+	dsn = pack.Message.Fields["dsn"].(string)
 
-	sentryMsg.parsed_dsn, sentryMsg.prep_error = url.Parse(sentryMsg.dsn)
-	if sentryMsg.prep_error != nil {
-		return sentryMsg.prep_error
+	sentryMsg.parsed_dsn, prep_error = url.Parse(dsn)
+	if prep_error != nil {
+		return fmt.Errorf("Error parsing DSN from sentry message")
 	}
 
-	sentryMsg.auth_header, sentryMsg.prep_error = sentryMsg.compute_auth_header()
+	auth_header, prep_error = sentryMsg.compute_auth_header()
 
-	if sentryMsg.prep_error != nil {
-		return sentryMsg.prep_error
+	if prep_error != nil {
+		return fmt.Errorf("Error computing authentication header from sentry message")
 	}
 
-	sentryMsg.data_packet = []byte(fmt.Sprintf("%s\n\n%s", sentryMsg.auth_header, sentryMsg.encoded_payload))
+	sentryMsg.data_packet = []byte(fmt.Sprintf("%s\n\n%s", auth_header, sentryMsg.encoded_payload))
 	return nil
 }
 
@@ -171,7 +150,7 @@ func (self *SentryOutputWriter) Write(outData interface{}) (err error) {
 	socket, host_ok = self.udpMap[udp_addr_str]
 	if !host_ok {
 		if len(self.udpMap) > self.config.MaxUdpSockets {
-			return SentryOutError{time.Now(), "Maximum number of UDP sockets reached."}
+			return fmt.Errorf("Maximum number of UDP sockets reached.")
 		}
 
 		udp_addr, socket_err = net.ResolveUDPAddr("udp", udp_addr_str)
@@ -181,7 +160,7 @@ func (self *SentryOutputWriter) Write(outData interface{}) (err error) {
 
 		socket, socket_err = net.DialUDP("udp", nil, udp_addr)
 		if socket_err != nil {
-			return SentryOutError{time.Now(), "Error while dialing the UDP socket"}
+			return fmt.Errorf("Error while dialing the UDP socket")
 		}
 		self.udpMap[sentryMsg.parsed_dsn.Host] = socket
 	}
